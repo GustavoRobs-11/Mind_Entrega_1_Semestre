bc5b53f300445c25c6f91993be3d0281092fd987
GustavoRobs-11 <gustavo.silva510@fatec.sp.gov.br>
2025-03-26 20:11:39 -0300

    aindanaofunciona

--- scripts/pesquisa.js
+++ scripts/pesquisa.js
@@ -5,10 +5,10 @@ function searchCards() {
         const query = searchInput.value.toLowerCase();
 
         cards.forEach(card => {
-            const title = card.getAttribute('data-title').toLowerCase();
-            const description = card.getAttribute('data-description').toLowerCase();
-            const info = card.getAttribute('psi-info').toLowerCase();
-            const stats = card.getAttribute('hm-psi-stats').toLowerCase();
+            const title = card.getAttribute('data-title').toLowerCase();  // Corrigido
+            const description = card.getAttribute('data-description').toLowerCase();  // Corrigido
+            const info = card.getAttribute('psi-info').toLowerCase();  // Corrigido
+            const stats = card.getAttribute('hm-psi-stats').toLowerCase();  // Corrigido
 
             if (title.includes(query) || description.includes(query) || info.includes(query) || stats.includes(query)) {
                 card.style.display = 'block';
